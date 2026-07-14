5746945fc6d448e0284a2984bb0c787b2a3dfe63
Diana Kravchuk <140897201+Wghaaj@users.noreply.github.com>
2026-07-14 11:42:55 +0100

    updated files

--- _next/static/uPGcFIfgAzFROmVnqD0mb/_buildManifest.js
+++ _next/static/uPGcFIfgAzFROmVnqD0mb/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/birthday-celebration//_next/:path+",
+        "destination": "/birthday-celebration/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
